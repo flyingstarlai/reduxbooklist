2a9af177990ab141031c19fedb6cdfa4e700d06b
Starlai <flyingstarlai@gmail.com>
2017-10-21 10:01:10 +0800

    first comm

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'Java Script: Evil Side' },
+        { title: 'Hairy Potter: Golden Cage'},
+        { title: 'Kingsman: Stupid Teddy'},
+        { title: 'Town Of Salem'},
+    ]
+}
